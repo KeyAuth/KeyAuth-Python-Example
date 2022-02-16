b2435c923ec4275efe07a50a05db7810630f9718
Weebzo <74221482+weebzo@users.noreply.github.com>
2022-02-15 17:02:59 -0800

    Updated Example

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 from keyauth import api
-
 import os
 import sys
 import os.path
 import platform
+from time import sleep
 from datetime import datetime
 
 # watch setup video if you need help https://www.youtube.com/watch?v=L2eAQOmuUiA
-
-keyauthapp = api("app name here", "owner id here", "app secret here","1.0")
-
+os.system("cls")
+os.system("title Python Example")
 print("Initializing")
-keyauthapp.init()
-
+keyauthapp = api(
+	name = "",
+	ownerid = "",
+	secret = "",
+	version = "1.0",
+)
 print ("""
 1.Login
 2.Register
 3.Upgrade
 4.License Key Only
 """)
+print(f"""
+App data:
+Number of users: {keyauthapp.app_data.numUsers}
+Number of online users: {keyauthapp.app_data.onlineUsers}
+Number of keys: {keyauthapp.app_data.numKeys}
+Application Version: {keyauthapp.app_data.app_ver}
+Customer panel link: {keyauthapp.app_data.customer_panel}
+""")
+sleep(1.5) # rate limit
 print(f"Current Session Validation Status: {keyauthapp.check()}")
+sleep(1.5) # rate limit
+print(f"Blacklisted? : {keyauthapp.checkblacklist()}") # check if blacklisted, you can edit this and make it exit the program if blacklisted
+
 ans=input("Select Option: ") 
 if ans=="1": 
 	user = input('Provide username: ')
@@ -45,7 +60,11 @@ print("\nUser data: ")
 print("Username: " + keyauthapp.user_data.username)
 print("IP address: " + keyauthapp.user_data.ip)
 print("Hardware-Id: " + keyauthapp.user_data.hwid)
+print("Subcription: " + keyauthapp.user_data.subscription)
 print("Created at: " + datetime.utcfromtimestamp(int(keyauthapp.user_data.createdate)).strftime('%Y-%m-%d %H:%M:%S'))
 print("Last login at: " + datetime.utcfromtimestamp(int(keyauthapp.user_data.lastlogin)).strftime('%Y-%m-%d %H:%M:%S'))
 print("Expires at: " + datetime.utcfromtimestamp(int(keyauthapp.user_data.expires)).strftime('%Y-%m-%d %H:%M:%S'))
 print(f"Current Session Validation Status: {keyauthapp.check()}")
+print("Exiting in 10 secs....")
+sleep(10)
+exit(0)
