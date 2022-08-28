a02b5a4681160657b0dea8573313e8a0a7a586fb
6nz <74118308+6nz@users.noreply.github.com>
2022-08-28 21:50:43 +0200

    Changed some lines
    instead of exiting  after printing "Not valid option" it just re call the function, bit cleaner file

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from keyauth import api
+import time
 import os
-import os.path
 import hashlib
 from time import sleep
 from datetime import datetime
 
-#import json as jsond 
-#^^ only for auto login/json writing/reading
+# import json as jsond
+# ^^ only for auto login/json writing/reading
 
 # watch setup video if you need help https://www.youtube.com/watch?v=L2eAQOmuUiA
 os.system("cls")
 os.system("title Python Example")
 print("Initializing")
+
+
 def getchecksum():
-	path = os.path.basename(__file__)
-	if not os.path.exists(path):
-		path = path[:-2] + "exe"
-	md5_hash = hashlib.md5()
-	a_file = open(path,"rb")
-	content = a_file.read()
-	md5_hash.update(content)
-	digest = md5_hash.hexdigest()
-	return digest
+    path = os.path.basename(__file__)
+    if not os.path.exists(path):
+        path = path[:-2] + "exe"
+    md5_hash = hashlib.md5()
+    a_file = open(path, "rb")
+    content = a_file.read()
+    md5_hash.update(content)
+    digest = md5_hash.hexdigest()
+    return digest
+
 
 keyauthapp = api(
     name = "", #App name (Manage Applications --> Application name)
@@ -40,220 +43,229 @@ Application Version: {keyauthapp.app_data.app_ver}
 Customer panel link: {keyauthapp.app_data.customer_panel}
 """)
 print(f"Current Session Validation Status: {keyauthapp.check()}")
-print(f"Blacklisted? : {keyauthapp.checkblacklist()}") # check if blacklisted, you can edit this and make it exit the program if blacklisted
-print ("""
+print(
+    f"Blacklisted? : {keyauthapp.checkblacklist()}")  # check if blacklisted, you can edit this and make it exit the program if blacklisted
+
+
+def answer():
+    try:
+        print("""
 1.Login
 2.Register
 3.Upgrade
 4.License Key Only
-""")
-ans=input("Select Option: ") 
-if ans=="1": 
-	user = input('Provide username: ')
-	password = input('Provide password: ')
-	keyauthapp.login(user,password)
-elif ans=="2":
-	user = input('Provide username: ')
-	password = input('Provide password: ')
-	license = input('Provide License: ')
-	keyauthapp.register(user,password,license) 
-elif ans=="3":
-	user = input('Provide username: ')
-	license = input('Provide License: ')
-	keyauthapp.upgrade(user,license)
-    
-elif ans=="4":
-	key = input('Enter your license: ')
-	keyauthapp.license(key)
-else:
-	print("\nNot Valid Option") 
-	os._exit(1)
+        """)
+        ans = input("Select Option: ")
+        if ans == "1":
+            user = input('Provide username: ')
+            password = input('Provide password: ')
+            keyauthapp.login(user, password)
+        elif ans == "2":
+            user = input('Provide username: ')
+            password = input('Provide password: ')
+            license = input('Provide License: ')
+            keyauthapp.register(user, password, license)
+        elif ans == "3":
+            user = input('Provide username: ')
+            license = input('Provide License: ')
+            keyauthapp.upgrade(user, license)
+        elif ans == "4":
+            key = input('Enter your license: ')
+            keyauthapp.license(key)
+        else:
+            print("\nNot Valid Option")
+            time.sleep(1)
+            os.system('cls')
+            answer()
+    except KeyboardInterrupt:
+        os._exit(1)
 
 
-#region Extra Functions
+answer()
 
-#* Download Files form the server to your computer using the download function in the api class
-#bytes = keyauthapp.download("FILEID")
-#f = open("example.exe", "wb")
-#f.write(bytes)
-#f.close()
+# region Extra Functions
 
-
-#* Set up user variable
-#keyauthapp.setvar("varName", "varValue")
-
-#* Get user variable and print it
-#data = keyauthapp.getvar("varName")
-#print(data)
-
-#* Get normal variable and print it
-#data = keyauthapp.var("varName")
-#print(data)
-
-#* Log message to the server and then to your webhook what is set on app settings
-#keyauthapp.log("Message")
-
-#* Get if the user pc have been blacklisted
-#print(f"Blacklisted? : {keyauthapp.checkblacklist()}")
-
-#* See if the current session is validated
-#print(f"Session Validated?: {keyauthapp.check()}")
+# * Download Files form the server to your computer using the download function in the api class
+# bytes = keyauthapp.download("FILEID")
+# f = open("example.exe", "wb")
+# f.write(bytes)
+# f.close()
 
 
-#* example to send normal request with no POST data
-#data = keyauthapp.webhook("WebhookID", "?type=resetuser&user=username")
+# * Set up user variable
+# keyauthapp.setvar("varName", "varValue")
 
-#* Get chat messages
-#messages = keyauthapp.chatGet("CHANNEL")
+# * Get user variable and print it
+# data = keyauthapp.getvar("varName")
+# print(data)
 
-#Messages = ""
-#for i in range(len(messages)):
-#	Messages += datetime.utcfromtimestamp(int(messages[i]["timestamp"])).strftime('%Y-%m-%d %H:%M:%S') + " - " + messages[i]["author"] + ": " + messages[i]["message"] + "\n"
+# * Get normal variable and print it
+# data = keyauthapp.var("varName")
+# print(data)
 
-#print("\n\n" + Messages)
+# * Log message to the server and then to your webhook what is set on app settings
+# keyauthapp.log("Message")
 
-#* Send chat message
-#keyauthapp.chatSend("MESSAGE", "CHANNEL")
+# * Get if the user pc have been blacklisted
+# print(f"Blacklisted? : {keyauthapp.checkblacklist()}")
+
+# * See if the current session is validated
+# print(f"Session Validated?: {keyauthapp.check()}")
 
 
-#* Auto-Login Example (THIS IS JUST AN EXAMPLE --> YOU WILL HAVE TO EDIT THE CODE PROBABLY)
-#1. Checking and Reading JSON
+# * example to send normal request with no POST data
+# data = keyauthapp.webhook("WebhookID", "?type=resetuser&user=username")
+
+# * Get chat messages
+# messages = keyauthapp.chatGet("CHANNEL")
+
+# Messages = ""
+# for i in range(len(messages)):
+# Messages += datetime.utcfromtimestamp(int(messages[i]["timestamp"])).strftime('%Y-%m-%d %H:%M:%S') + " - " + messages[i]["author"] + ": " + messages[i]["message"] + "\n"
+
+# print("\n\n" + Messages)
+
+# * Send chat message
+# keyauthapp.chatSend("MESSAGE", "CHANNEL")
+
+
+# * Auto-Login Example (THIS IS JUST AN EXAMPLE --> YOU WILL HAVE TO EDIT THE CODE PROBABLY)
+# 1. Checking and Reading JSON
 
 #### Note: Remove the ''' on line 124 and 183
 
 '''try:
-	if os.path.isfile('auth.json'): #Checking if the auth file exist
-		if jsond.load(open("auth.json"))["authusername"] == "": #Checks if the authusername is empty or not
-			print("""
+    if os.path.isfile('auth.json'): #Checking if the auth file exist
+        if jsond.load(open("auth.json"))["authusername"] == "": #Checks if the authusername is empty or not
+            print("""
 1. Login
 2. Register
-			""")
-			ans=input("Select Option: ")  #Skipping auto-login bc auth file is empty
-			if ans=="1": 
-				user = input('Provide username: ')
-				password = input('Provide password: ')
-				keyauthapp.login(user,password)
-			elif ans=="2":
-				user = input('Provide username: ')
-				password = input('Provide password: ')
-				license = input('Provide License: ')
-				keyauthapp.register(user,password,license) 
-			else:
-				print("\nNot Valid Option") 
-				os._exit(1) 
-		else:
-			try: #2. Auto login
-				with open('auth.json', 'r') as f:
-					authfile = jsond.load(f)
-					authuser = authfile.get('authusername')
-					authpass = authfile.get('authpassword')
-					keyauthapp.login(authuser,authpass)
-			except Exception as e: #Error stuff
-				print(e)
-	else: #Creating auth file bc its missing
-		try:
-			f = open("auth.json", "a") #Writing content
-			f.write("""{
-	"authusername": "",
-	"authpassword": ""
+            """)
+            ans=input("Select Option: ")  #Skipping auto-login bc auth file is empty
+            if ans=="1": 
+                user = input('Provide username: ')
+                password = input('Provide password: ')
+                keyauthapp.login(user,password)
+            elif ans=="2":
+                user = input('Provide username: ')
+                password = input('Provide password: ')
+                license = input('Provide License: ')
+                keyauthapp.register(user,password,license) 
+            else:
+                print("\nNot Valid Option") 
+                os._exit(1) 
+        else:
+            try: #2. Auto login
+                with open('auth.json', 'r') as f:
+                    authfile = jsond.load(f)
+                    authuser = authfile.get('authusername')
+                    authpass = authfile.get('authpassword')
+                    keyauthapp.login(authuser,authpass)
+            except Exception as e: #Error stuff
+                print(e)
+    else: #Creating auth file bc its missing
+        try:
+            f = open("auth.json", "a") #Writing content
+            f.write("""{
+    "authusername": "",
+    "authpassword": ""
 }""")
-			f.close()
-			print ("""
+            f.close()
+            print ("""
 1. Login
 2. Register
-			""")#Again skipping auto-login bc the file is empty/missing
-			ans=input("Select Option: ") 
-			if ans=="1": 
-				user = input('Provide username: ')
-				password = input('Provide password: ')
-				keyauthapp.login(user,password)
-			elif ans=="2":
-				user = input('Provide username: ')
-				password = input('Provide password: ')
-				license = input('Provide License: ')
-				keyauthapp.register(user,password,license) 
-			else:
-				print("\nNot Valid Option") 
-				os._exit(1) 
-		except Exception as e: #Error stuff
-			print(e)
-			os._exit(1) 
+            """)#Again skipping auto-login bc the file is empty/missing
+            ans=input("Select Option: ") 
+            if ans=="1": 
+                user = input('Provide username: ')
+                password = input('Provide password: ')
+                keyauthapp.login(user,password)
+            elif ans=="2":
+                user = input('Provide username: ')
+                password = input('Provide password: ')
+                license = input('Provide License: ')
+                keyauthapp.register(user,password,license) 
+            else:
+                print("\nNot Valid Option") 
+                os._exit(1) 
+        except Exception as e: #Error stuff
+            print(e)
+            os._exit(1) 
 except Exception as e: #Error stuff
-	print(e)
-	os._exit(1)'''
+    print(e)
+    os._exit(1)'''
 
-
-#Writing user data on login:
-#Check Keyauth.py file --> Line 152
-#Replace the whole login function with this login function (This has auto user data dumping, so the user only have to login once)
-#Note: The auto login function above is needed for this bc it creates the auth file, if the auth file is missing this wont work
+# Writing user data on login:
+# Check Keyauth.py file --> Line 152
+# Replace the whole login function with this login function (This has auto user data dumping, so the user only have to login once)
+# Note: The auto login function above is needed for this bc it creates the auth file, if the auth file is missing this won't work
 
 '''def login(self, user, password, hwid=None):
-	self.checkinit()
-	if hwid is None:
-		hwid = others.get_hwid()
+    self.checkinit()
+    if hwid is None:
+        hwid = others.get_hwid()
 
-	init_iv = SHA256.new(str(uuid4())[:8].encode()).hexdigest()
+    init_iv = SHA256.new(str(uuid4())[:8].encode()).hexdigest()
 
-	post_data = {
-		"type": binascii.hexlify(("login").encode()),
-		"username": encryption.encrypt(user, self.enckey, init_iv),
-		"pass": encryption.encrypt(password, self.enckey, init_iv),
-		"hwid": encryption.encrypt(hwid, self.enckey, init_iv),
-		"sessionid": binascii.hexlify(self.sessionid.encode()),
-		"name": binascii.hexlify(self.name.encode()),
-		"ownerid": binascii.hexlify(self.ownerid.encode()),
-		"init_iv": init_iv
-	}
+    post_data = {
+        "type": binascii.hexlify(("login").encode()),
+        "username": encryption.encrypt(user, self.enckey, init_iv),
+        "pass": encryption.encrypt(password, self.enckey, init_iv),
+        "hwid": encryption.encrypt(hwid, self.enckey, init_iv),
+        "sessionid": binascii.hexlify(self.sessionid.encode()),
+        "name": binascii.hexlify(self.name.encode()),
+        "ownerid": binascii.hexlify(self.ownerid.encode()),
+        "init_iv": init_iv
+    }
 
-	response = self.__do_request(post_data)
+    response = self.__do_request(post_data)
 
-	response = encryption.decrypt(response, self.enckey, init_iv)
+    response = encryption.decrypt(response, self.enckey, init_iv)
 
-	json = jsond.loads(response)
+    json = jsond.loads(response)
 
-	if json["success"]:
-		self.__load_user_data(json["info"])
-		if jsond.load(open("auth.json"))["authusername"] == "": #Check if the authusername is empty so it can write the user data
-			authfile = jsond.load(open("Files/auth.json"))
-			authfile["authusername"] = user #login(self, user)
-			jsond.dump(authfile, open('Files/auth.json', 'w'), sort_keys=False, indent=4) #Dumping username to auth file/You can change the indent
-			authfile["authpassword"] = password #login(self, password)
-			jsond.dump(authfile, open('Files/auth.json', 'w'), sort_keys=False, indent=4) #Dumping password to auth file/You can change the indent 
-		else: #Auth file is filled with data so it skips the user data dumping
-			pass
-		print("successfully logged in")
-	else:
-		print(json["message"])
-		os._exit(1)'''
+    if json["success"]:
+        self.__load_user_data(json["info"])
+        if jsond.load(open("auth.json"))["authusername"] == "": #Check if the authusername is empty so it can write the user data
+            authfile = jsond.load(open("Files/auth.json"))
+            authfile["authusername"] = user #login(self, user)
+            jsond.dump(authfile, open('Files/auth.json', 'w'), sort_keys=False, indent=4) #Dumping username to auth file/You can change the indent
+            authfile["authpassword"] = password #login(self, password)
+            jsond.dump(authfile, open('Files/auth.json', 'w'), sort_keys=False, indent=4) #Dumping password to auth file/You can change the indent 
+        else: #Auth file is filled with data so it skips the user data dumping
+            pass
+        print("successfully logged in")
+    else:
+        print(json["message"])
+        os._exit(1)'''
 
-#endregion
+# endregion
 
 
-print("\nUser data: ") 
+print("\nUser data: ")
 print("Username: " + keyauthapp.user_data.username)
 print("IP address: " + keyauthapp.user_data.ip)
 print("Hardware-Id: " + keyauthapp.user_data.hwid)
-#print("Subcription: " + keyauthapp.user_data.subscription) ## Print Subscription "ONE" name
+# print("Subcription: " + keyauthapp.user_data.subscription) ## Print Subscription "ONE" name
 
-subs = keyauthapp.user_data.subscriptions ## Get all Subscription names, expiry, and timeleft
+subs = keyauthapp.user_data.subscriptions  # Get all Subscription names, expiry, and timeleft
 for i in range(len(subs)):
-  sub = subs[i]["subscription"] # Subscription from every Sub
-  expiry = datetime.utcfromtimestamp(int(subs[i]["expiry"])).strftime('%Y-%m-%d %H:%M:%S') ## Expiry date from every Sub
-  timeleft = subs[i]["timeleft"] ## Timeleft from every Sub
+    sub = subs[i]["subscription"]  # Subscription from every Sub
+    expiry = datetime.utcfromtimestamp(int(subs[i]["expiry"])).strftime(
+        '%Y-%m-%d %H:%M:%S')  # Expiry date from every Sub
+    timeleft = subs[i]["timeleft"]  # Timeleft from every Sub
 
-  print(f"[{i + 1} / {len(subs)}] | Subscription: {sub} - Expiry: {expiry} - Timeleft: {timeleft}")
+    print(f"[{i + 1} / {len(subs)}] | Subscription: {sub} - Expiry: {expiry} - Timeleft: {timeleft}")
 
 onlineUsers = keyauthapp.fetchOnline()
-OU = "" ## KEEP THIS EMPTY FOR NOW, THIS WILL BE USED TO CREATE ONLINE USER STRING.
-if onlineUsers == None: 
-	OU = "No online users"
+OU = ""  # KEEP THIS EMPTY FOR NOW, THIS WILL BE USED TO CREATE ONLINE USER STRING.
+if onlineUsers is None:
+    OU = "No online users"
 else:
-	for i in range(len(onlineUsers)):
-	    OU += onlineUsers[i]["credential"] + " "
+    for i in range(len(onlineUsers)):
+        OU += onlineUsers[i]["credential"] + " "
 
 print("\n" + OU + "\n")
-
 
 print("Created at: " + datetime.utcfromtimestamp(int(keyauthapp.user_data.createdate)).strftime('%Y-%m-%d %H:%M:%S'))
 print("Last login at: " + datetime.utcfromtimestamp(int(keyauthapp.user_data.lastlogin)).strftime('%Y-%m-%d %H:%M:%S'))
