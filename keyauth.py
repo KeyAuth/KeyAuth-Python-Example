fb08c628e6679f3808eae70deeb5898f8fc01a0f
api <110469682+apilol@users.noreply.github.com>
2023-04-10 00:23:58 -0700

    Change get hwid function

--- keyauth.py
+++ keyauth.py
@@ -7,8 +7,6 @@ import platform  # check platform
 import subprocess  # needed for mac device
 
 try:
-    if os.name == 'nt':
-        import win32security  # get sid (WIN only)
     import requests  # https requests
     from Crypto.Cipher import AES
     from Crypto.Hash import SHA256
@@ -573,10 +571,17 @@ class others:
                 hwid = f.read()
                 return hwid
         elif platform.system() == 'Windows':
-            winuser = os.getlogin()
-            sid = win32security.LookupAccountName(None, winuser)[0]  # You can also use WMIC (better than SID, some users had problems with WMIC)
-            hwid = win32security.ConvertSidToStringSid(sid)
-            return hwid
+            cmd = subprocess.Popen(
+                "wmic useraccount where name='%username%' get sid",
+                stdout=subprocess.PIPE,
+                shell=True,
+            )
+
+            (suppost_sid, error) = cmd.communicate()
+
+            suppost_sid = suppost_sid.split(b"\n")[1].strip()
+
+            return suppost_sid.decode()
         elif platform.system() == 'Darwin':
             output = subprocess.Popen("ioreg -l | grep IOPlatformSerialNumber", stdout=subprocess.PIPE, shell=True).communicate()[0]
             serial = output.decode().split('=', 1)[1].replace(' ', '')
